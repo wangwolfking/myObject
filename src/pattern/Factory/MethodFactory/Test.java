9b4c99eea901ce2192f1170b38c9822e227827e6
yocalywzg <yocalywzg@yocalywzg-PC>
2017-12-14 16:33:43 +0800

    工厂测试类

---
+++ src/pattern/Factory/MethodFactory/Test.java
@@ -0,0 +1,15 @@
+package pattern.Factory.MethodFactory;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Factory fa = new CreateAirCondition();
+		fa.create();
+		Factory fb = new CreateIcebox();
+		fb.create();
+		Factory fc = new CreateWasher();
+		fc.create();
+
+	}
+
+}
